e6adcc621a4d7a3e2864174f79627357fab2710f
jaimin25 <jaiminchovatia25123@gmail.com>
2024-01-22 20:56:12 +0530

    delete cha

--- src/components/modals/conversation-info-modal.tsx
+++ src/components/modals/conversation-info-modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Info, ShieldAlert } from 'lucide-react';
 
 import {
@@ -13,6 +13,7 @@ import {
   DialogTrigger
 } from '@/components/ui/dialog';
 import { joinTimeFormat } from '@/lib/joinTimeFormat';
+import { TrashIcon } from '@heroicons/react/24/outline';
 
 import { ChatsProps } from '../providers/chats-provider';
 import { useUser } from '../providers/user-provider';
@@ -20,11 +21,12 @@ import { Button } from '../ui/button';
 import UserAvatar from '../user-avatar';
 
 export default function ConvoInfoModal({ chat }: { chat: ChatsProps }) {
+  const [confirmDelete, setConfirmDelete] = useState<boolean>(false);
   const date = chat && new Date(chat.createdAt);
   const { user } = useUser();
 
   return (
-    <Dialog>
+    <Dialog onOpenChange={() => setConfirmDelete(false)}>
       <DialogTrigger className="mx-2 ml-auto rounded-full p-2 hover:cursor-pointer hover:bg-white/15">
         <div>
           <Info className="h-6 w-6" />
@@ -32,49 +34,102 @@ export default function ConvoInfoModal({ chat }: { chat: ChatsProps }) {
       </DialogTrigger>
       <DialogContent className="border-0">
         <DialogHeader className="text-left">
-          <DialogTitle>Conversation Info</DialogTitle>
+          <DialogTitle>
+            {!confirmDelete ? 'Conversation Info' : 'Delete Chat'}
+          </DialogTitle>
           <DialogDescription>
-            <p className="">
-              Created on{' '}
-              {chat && date.toDateString() + ' at ' + date.toLocaleTimeString()}
-            </p>
-            <p className="text-xl font-semibold text-white">Members:</p>
-            {chat &&
-              chat.users.map((chatUser) => {
-                const joinedAtTime = joinTimeFormat(chatUser.createdAt);
+            {!confirmDelete ? (
+              <>
+                <p className="">
+                  Created on{' '}
+                  {chat &&
+                    date.toDateString() + ' at ' + date.toLocaleTimeString()}
+                </p>
+                <p className="text-xl font-semibold text-white">Members:</p>
+                {chat &&
+                  chat.users.map((chatUser) => {
+                    const joinedAtTime = joinTimeFormat(chatUser.createdAt);
 
-                return (
-                  user.id !== chatUser.id && (
-                    <div key={user.id}>
-                      <div className="group flex items-center gap-x-4 rounded p-1 px-2 transition hover:cursor-pointer hover:bg-white/15">
-                        <div className="flex h-full items-center justify-center">
-                          <UserAvatar
-                            className="m-3 h-11 w-11 rounded-md"
-                            username={chatUser.name}
-                          />
-                        </div>
-                        <div className="flex w-full flex-col items-start">
-                          <p className="flex items-center gap-x-1 text-lg">
-                            {chatUser.name}
-                            {chatUser.id === chat.adminId && (
-                              <ShieldAlert className="h-5 w-5 text-indigo-500" />
-                            )}
-                          </p>
-                          <p className="text-xs text-stone-400">
-                            Joined {joinedAtTime} ago
-                          </p>
+                    return (
+                      <div key={user.id}>
+                        <div className="group flex items-center gap-x-4 rounded p-1 px-2 transition hover:cursor-pointer hover:bg-white/15">
+                          <div className="flex h-full items-center justify-center">
+                            <UserAvatar
+                              className="m-3 h-11 w-11 rounded-md"
+                              username={chatUser.name}
+                            />
+                          </div>
+                          <div className="flex w-full flex-col items-start">
+                            <p className="flex items-center gap-x-1 text-lg">
+                              {user.id === chatUser.id && '(You) '}
+                              {chatUser.name}
+                              {chatUser.id === chat.adminId && (
+                                <ShieldAlert className="h-5 w-5 text-indigo-500" />
+                              )}
+                            </p>
+                            <p className="text-xs text-stone-400">
+                              Joined {joinedAtTime} ago
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  )
-                );
-              })}
+                    );
+                  })}{' '}
+              </>
+            ) : (
+              <p>
+                Are you sure, you want to delete this chat? This action will
+                also delete chat for the participants in it.
+              </p>
+            )}
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button variant={'ghost'} className="space-x-1 bg-red-500">
-            <p>Close</p>
-          </Button>
+          {chat.isGroup && chat.adminId === user.id ? (
+            confirmDelete ? (
+              <>
+                <Button
+                  onClick={() => setConfirmDelete(false)}
+                  variant={'secondary'}
+                  className="mt-2 sm:mt-0"
+                >
+                  Cancel
+                </Button>
+                <Button variant={'destructive'}>Confirm</Button>
+              </>
+            ) : (
+              <Button
+                variant={'ghost'}
+                className="space-x-1 bg-red-500"
+                onClick={() => setConfirmDelete(true)}
+              >
+                <TrashIcon className="h-5 w-5" />
+                <p>Delete</p>
+              </Button>
+            )
+          ) : (
+            !chat.isGroup &&
+            (confirmDelete ? (
+              <>
+                <Button
+                  onClick={() => setConfirmDelete(false)}
+                  variant={'secondary'}
+                >
+                  Cancel
+                </Button>
+                <Button variant={'destructive'}>Confirm</Button>
+              </>
+            ) : (
+              <Button
+                variant={'ghost'}
+                className="space-x-1 bg-red-500"
+                onClick={() => setConfirmDelete(true)}
+              >
+                <TrashIcon className="h-5 w-5" />
+                <p>Delete</p>
+              </Button>
+            ))
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
