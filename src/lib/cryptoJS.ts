5a40695bbd9f2978cb5a529fe6b867500458cac4
jaimin25 <jaiminchovatia25123@gmail.com>
2024-02-04 13:07:40 +0530

    changed key

--- src/lib/cryptoJS.ts
+++ src/lib/cryptoJS.ts
@@ -3,13 +3,13 @@ import CryptoJS from 'crypto-js';
 export function encryptValue(val: string) {
   return CryptoJS.AES.encrypt(
     val,
-    process.env.NEXT_PUBLIC_CRYPTOJS_SECRET_KEY as string
+    process.env.CRYPTOJS_SECRET_KEY as string
   ).toString();
 }
 
 export function decryptValue(val: string) {
   return CryptoJS.AES.decrypt(
     val,
-    process.env.NEXT_PUBLIC_CRYPTOJS_SECRET_KEY as string
+    process.env.CRYPTOJS_SECRET_KEY as string
   ).toString(CryptoJS.enc.Utf8);
 }
