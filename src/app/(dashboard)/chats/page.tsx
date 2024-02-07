b05fd2d6798e62454b8f980327501a1fb6f50409
jaimin25 <jaiminchovatia25123@gmail.com>
2024-02-07 21:04:10 +0530

    fix: stlying

--- src/app/(dashboard)/chats/page.tsx
+++ src/app/(dashboard)/chats/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from '@/components/ui/card';
 
 export default function ChatsPage() {
   return (
-    <div className="mr-4 h-full w-full flex-1 py-3">
+    <div className="mr-4 h-full w-full flex-1 py-3 pl-4 md:pl-0">
       <Card className="h-full w-full border-0 px-3 dark:bg-black/50">
         <CardContent className="h-full">
           <div className="flex h-full w-full flex-col items-center justify-center gap-y-2 text-center">
