1f35b7053377f00d15a656c07f54263502a00c6a
jaimin25 <jaiminchovatia25123@gmail.com>
2024-08-17 21:50:47 +0530

    fix: unused useState

--- src/components/chats/chatbox/message-list.tsx
+++ src/components/chats/chatbox/message-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import axios from 'axios';
 
