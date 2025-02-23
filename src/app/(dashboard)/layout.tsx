6796d26de4712da88bc82ae80c9d56678635e18b
Jaimin25 <chovatiajaimin@gmail.com>
2025-02-23 16:20:06 +0530

    feat: add metadata and icons for improved link previews (#2)

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -20,7 +20,36 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'ConvoNest',
-  description: 'Place to have some fun conversations'
+  description: 'Place to have some fun conversations',
+  openGraph: {
+    title: 'ConvoNest',
+    description:
+      'ConvoNest is a realtime web application for chatting, where you can add friends and chat with them individually or in a group.',
+    images: [
+      {
+        url: `https://raw.githubusercontent.com/Jaimin25/ConvoNest/refs/heads/main/public/Link%20Preview%20-%20ConvoNest.png`,
+        width: 1200,
+        height: 630,
+        alt: 'DayDonezo'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@jaimin_chovatia',
+    creator: '@jaimin_chovatia',
+    title: 'ConvoNest - Chat App',
+    description:
+      'ConvoNest is a realtime web application for chatting, where you can add friends and chat with them individually or in a group.',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/Jaimin25/ConvoNest/refs/heads/main/public/Link%20Preview%20-%20ConvoNest.png',
+        width: 1200,
+        height: 630,
+        alt: 'ConvoNest'
+      }
+    ]
+  }
 };
 export default function RootLayout({
   children
